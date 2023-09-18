0317ced4184fe4ddabf93bd30711a5426cf4370d
Claudel-D-Roy <112507354+Claudel-D-Roy@users.noreply.github.com>
2023-09-18 19:53:05 -0400

    push About us

--- ClientApp/src/components/Aboutus.js
+++ ClientApp/src/components/Aboutus.js
@@ -4,38 +4,58 @@ function AboutUs() {
     const Membres = [
         {
             name: 'Merlin Gélinas',
+            role: '',
             imgageSrc: '/'
         },
         {
             name: 'Mathieu Duval',
+            role: '',
             imgageSrc: '/'
         },
         {
             name: 'Gabriel Bruneau',
+            role: '',
             imgageSrc: '/'
         },
         {
             name: 'Kyle Lussier',
+            role: '',
             imgageSrc: '/'
         },
         {
             name: 'Thomas-Alexandre Barrette',
+            role: '',
             imgageSrc: '/'
         },
         {
             name: 'Claudel D. Roy',
+            role: '',
             imgageSrc: '/'
         },
 
 
     ];
-}
 
-function AboutUs() {
+
+
     return (
        
-    
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to our company's About Us page. We are...</p>
+
+            <div className="team">
+                {Membres.map((member, index) => (
+                    <div key={index} className="team-member">
+                        <img src={member.imageSrc} alt={member.name} />
+                        <h2>{member.name}</h2>
+                        <p>{member.role}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
+  
 }
 
 export default AboutUs;
