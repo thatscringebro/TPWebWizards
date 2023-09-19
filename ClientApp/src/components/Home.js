4a0d47d897fd05197d7100bfe8ea7df05a53aea3
Mathieu Duval <9343789@etu.cegepsth.qc.ca>
2023-09-18 21:51:42 -0400

    push catégories

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, Form, FormGroup, Label, Input, Button, Alert } from 'reactstrap';
-
+import './Home.css';
 
 export class Home extends Component {
     static displayName = Home.name;
@@ -60,7 +60,7 @@ export class Home extends Component {
           </Container>
         </section>
 
-            
+            <hr className="my-divider" />   
 
         <section className="featured-products">
           <Container>
@@ -103,47 +103,221 @@ export class Home extends Component {
           </Container>
         </section>
 
-            <section className="partners">
-                <Container>
-                    <h2>Our Partners</h2>
-                    <Row>
-                        <Col md={2}>
-                            <img src="../../../images/partners/adisq.png" alt="Partner 1" />
-                        </Col>
-                        <Col md={2}>
-                            <img src="../../../images/partners/desjardins.png" alt="Partner 2" />
-                        </Col>
-                        <Col md={2}>
-                            <img src="../../../images/partners/vice.png" alt="Partner 3" />
-                        </Col>
-                    </Row>
-                </Container>
+            <hr className="my-divider" />
+
+        <section className="new-vinyl">
+            <Container>
+                <h2>New vinyl</h2>
+                <Row>
+                    <Col md={4}>
+                        <Card>
+                            <CardImg top src={require("./images/CD/Essenger-AfterDark.png")} alt="Product 1" style={{ maxHeight: '200px', width: 'auto', objectFit: 'contain' }} />
+                            <CardBody>
+                                <CardTitle>Product 1</CardTitle>
+                                <CardSubtitle>$19.99</CardSubtitle>
+                                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</CardText>
+                                <a href="/product/1" className="btn btn-secondary">Details</a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col md={4}>
+                        <Card>
+                            <CardImg top src={require("./images/Vinyl/Fushitsusha-1st.png")} alt="Product 2" style={{ maxHeight: '200px', width: 'auto', objectFit: 'contain' }} />
+                            <CardBody>
+                                <CardTitle>Product 2</CardTitle>
+                                <CardSubtitle>$24.99</CardSubtitle>
+                                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</CardText>
+                                <a href="/product/2" className="btn btn-secondary">Details</a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col md={4}>
+                        <Card>
+                            <CardImg top src={require("./images/Vinyl/MakeThemSuffer-HowToSurviveAFuneral.png")} alt="Product 3" style={{ maxHeight: '200px', width: 'auto', objectFit: 'contain' }} />
+                            <CardBody>
+                                <CardTitle>Product 3</CardTitle>
+                                <CardSubtitle>$29.99</CardSubtitle>
+                                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</CardText>
+                                <a href="/product/3" className="btn btn-secondary">Details</a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+
+            <hr className="my-divider" />
+
+        <section className="new-compact-disc">
+            <Container>
+                <h2>New CD</h2>
+                <Row>
+                    <Col md={4}>
+                        <Card>
+                            <CardImg top src={require("./images/CD/Essenger-AfterDark.png")} alt="Product 1" style={{ maxHeight: '200px', width: 'auto', objectFit: 'contain' }} />
+                            <CardBody>
+                                <CardTitle>Product 1</CardTitle>
+                                <CardSubtitle>$19.99</CardSubtitle>
+                                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</CardText>
+                                <a href="/product/1" className="btn btn-secondary">Details</a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col md={4}>
+                        <Card>
+                            <CardImg top src={require("./images/Vinyl/Fushitsusha-1st.png")} alt="Product 2" style={{ maxHeight: '200px', width: 'auto', objectFit: 'contain' }} />
+                            <CardBody>
+                                <CardTitle>Product 2</CardTitle>
+                                <CardSubtitle>$24.99</CardSubtitle>
+                                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</CardText>
+                                <a href="/product/2" className="btn btn-secondary">Details</a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col md={4}>
+                        <Card>
+                            <CardImg top src={require("./images/Vinyl/MakeThemSuffer-HowToSurviveAFuneral.png")} alt="Product 3" style={{ maxHeight: '200px', width: 'auto', objectFit: 'contain' }} />
+                            <CardBody>
+                                <CardTitle>Product 3</CardTitle>
+                                <CardSubtitle>$29.99</CardSubtitle>
+                                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</CardText>
+                                <a href="/product/3" className="btn btn-secondary">Details</a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
             </section>
 
-            {/* Ajout de la section du formulaire */}
-            <section className="contact-form">
-                <Container>
-                    <h2>Contact Us</h2>
-                    <Form onSubmit={this.handleSubmit}>
-                        <FormGroup>
-                            <Label for="name">Name</Label>
-                            <Input type="text" name="name" id="name" placeholder="Enter your name" value={this.state.formData.name} onChange={this.handleInputChange} />
-                            {this.state.errors.name && <Alert color="danger">{this.state.errors.name}</Alert>}
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="email">Email</Label>
-                            <Input type="email" name="email" id="email" placeholder="Enter your email" value={this.state.formData.email} onChange={this.handleInputChange} />
-                            {this.state.errors.email && <Alert color="danger">{this.state.errors.email}</Alert>}
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="description">Description</Label>
-                            <Input type="textarea" name="description" id="description" placeholder="Describe your request" value={this.state.formData.description} onChange={this.handleInputChange} />
-                            {this.state.errors.description && <Alert color="danger">{this.state.errors.description}</Alert>}
-                        </FormGroup>
-                        <Button type="submit">Submit</Button>
-                    </Form>
-                </Container>
+            <hr className="my-divider" />
+
+        <section className="used-vinyl">
+            <Container>
+                <h2>Used vinyl</h2>
+                <Row>
+                    <Col md={4}>
+                        <Card>
+                            <CardImg top src={require("./images/CD/Essenger-AfterDark.png")} alt="Product 1" style={{ maxHeight: '200px', width: 'auto', objectFit: 'contain' }} />
+                            <CardBody>
+                                <CardTitle>Product 1</CardTitle>
+                                <CardSubtitle>$19.99</CardSubtitle>
+                                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</CardText>
+                                <a href="/product/1" className="btn btn-secondary">Details</a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col md={4}>
+                        <Card>
+                            <CardImg top src={require("./images/Vinyl/Fushitsusha-1st.png")} alt="Product 2" style={{ maxHeight: '200px', width: 'auto', objectFit: 'contain' }} />
+                            <CardBody>
+                                <CardTitle>Product 2</CardTitle>
+                                <CardSubtitle>$24.99</CardSubtitle>
+                                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</CardText>
+                                <a href="/product/2" className="btn btn-secondary">Details</a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col md={4}>
+                        <Card>
+                            <CardImg top src={require("./images/Vinyl/MakeThemSuffer-HowToSurviveAFuneral.png")} alt="Product 3" style={{ maxHeight: '200px', width: 'auto', objectFit: 'contain' }} />
+                            <CardBody>
+                                <CardTitle>Product 3</CardTitle>
+                                <CardSubtitle>$29.99</CardSubtitle>
+                                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</CardText>
+                                <a href="/product/3" className="btn btn-secondary">Details</a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
             </section>
+
+            <hr className="my-divider" />
+
+        <section className="used-compact-disc">
+            <Container>
+                <h2>Used CDs</h2>
+                <Row>
+                    <Col md={4}>
+                        <Card>
+                            <CardImg top src={require("./images/CD/Essenger-AfterDark.png")} alt="Product 1" style={{ maxHeight: '200px', width: 'auto', objectFit: 'contain' }} />
+                            <CardBody>
+                                <CardTitle>Product 1</CardTitle>
+                                <CardSubtitle>$19.99</CardSubtitle>
+                                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</CardText>
+                                <a href="/product/1" className="btn btn-secondary">Details</a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col md={4}>
+                        <Card>
+                            <CardImg top src={require("./images/Vinyl/Fushitsusha-1st.png")} alt="Product 2" style={{ maxHeight: '200px', width: 'auto', objectFit: 'contain' }} />
+                            <CardBody>
+                                <CardTitle>Product 2</CardTitle>
+                                <CardSubtitle>$24.99</CardSubtitle>
+                                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</CardText>
+                                <a href="/product/2" className="btn btn-secondary">Details</a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col md={4}>
+                        <Card>
+                            <CardImg top src={require("./images/Vinyl/MakeThemSuffer-HowToSurviveAFuneral.png")} alt="Product 3" style={{ maxHeight: '200px', width: 'auto', objectFit: 'contain' }} />
+                            <CardBody>
+                                <CardTitle>Product 3</CardTitle>
+                                <CardSubtitle>$29.99</CardSubtitle>
+                                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</CardText>
+                                <a href="/product/3" className="btn btn-secondary">Details</a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+
+            <hr className="my-divider" />
+
+        <section className="partners">
+            <Container>
+                <h2>Our Partners</h2>
+                <Row>
+                    <Col md={2}>
+                        <img src="../../../images/partners/adisq.png" alt="Partner 1" />
+                    </Col>
+                    <Col md={2}>
+                        <img src="../../../images/partners/desjardins.png" alt="Partner 2" />
+                    </Col>
+                    <Col md={2}>
+                        <img src="../../../images/partners/vice.png" alt="Partner 3" />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+
+        {/* Ajout de la section du formulaire */}
+        <section className="contact-form">
+            <Container>
+                <h2>Contact Us</h2>
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Label for="name">Name</Label>
+                        <Input type="text" name="name" id="name" placeholder="Enter your name" value={this.state.formData.name} onChange={this.handleInputChange} />
+                        {this.state.errors.name && <Alert color="danger">{this.state.errors.name}</Alert>}
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email">Email</Label>
+                        <Input type="email" name="email" id="email" placeholder="Enter your email" value={this.state.formData.email} onChange={this.handleInputChange} />
+                        {this.state.errors.email && <Alert color="danger">{this.state.errors.email}</Alert>}
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="description">Description</Label>
+                        <Input type="textarea" name="description" id="description" placeholder="Describe your request" value={this.state.formData.description} onChange={this.handleInputChange} />
+                        {this.state.errors.description && <Alert color="danger">{this.state.errors.description}</Alert>}
+                    </FormGroup>
+                    <Button type="submit">Submit</Button>
+                </Form>
+            </Container>
+        </section>
             
       </div>
     );
