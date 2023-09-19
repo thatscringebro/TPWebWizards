4f482356f4e39b4b121e68b4498bfe99c89d481a
thatscringebro <thatscringebro@tutanota.com>
2023-09-18 21:58:40 -0400

    new footer baby

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -102,7 +102,6 @@ export class Home extends Component {
             </Row>
           </Container>
         </section>
-
             <section className="partners">
                 <Container>
                     <h2>Our Partners</h2>
@@ -119,32 +118,6 @@ export class Home extends Component {
                     </Row>
                 </Container>
             </section>
-
-            {/* Ajout de la section du formulaire */}
-            <section className="contact-form">
-                <Container>
-                    <h2>Contact Us</h2>
-                    <Form onSubmit={this.handleSubmit}>
-                        <FormGroup>
-                            <Label for="name">Name</Label>
-                            <Input type="text" name="name" id="name" placeholder="Enter your name" value={this.state.formData.name} onChange={this.handleInputChange} />
-                            {this.state.errors.name && <Alert color="danger">{this.state.errors.name}</Alert>}
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="email">Email</Label>
-                            <Input type="email" name="email" id="email" placeholder="Enter your email" value={this.state.formData.email} onChange={this.handleInputChange} />
-                            {this.state.errors.email && <Alert color="danger">{this.state.errors.email}</Alert>}
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="description">Description</Label>
-                            <Input type="textarea" name="description" id="description" placeholder="Describe your request" value={this.state.formData.description} onChange={this.handleInputChange} />
-                            {this.state.errors.description && <Alert color="danger">{this.state.errors.description}</Alert>}
-                        </FormGroup>
-                        <Button type="submit">Submit</Button>
-                    </Form>
-                </Container>
-            </section>
-            
       </div>
     );
   }
