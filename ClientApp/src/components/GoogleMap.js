1b6d317a15d7310a6077bada3af7eb6a18867413
Claudel-D-Roy <112507354+Claudel-D-Roy@users.noreply.github.com>
2023-09-18 22:21:59 -0400

    GoogleMap, ApiKey, address info

---
+++ ClientApp/src/components/GoogleMap.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+function GoogleMapComponent() {
+    const mapStyles = {
+        height: '300px',
+        width: '50%',
+    };
+
+    const defaultCenter = {
+        lat: 53.00, 
+        lng: -70.00, 
+    };
+
+    return (
+        <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+            <GoogleMap mapContainerStyle={mapStyles} center={defaultCenter} zoom={10}>
+                <Marker position={defaultCenter} />
+            </GoogleMap>
+        </LoadScript>
+    );
+}
+
+export default GoogleMapComponent;
