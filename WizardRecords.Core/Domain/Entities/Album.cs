2d30fd528a84ccbbd666b7ae4c3259cde1c03fd7
Mathieu Duval <9343789@etu.cegepsth.qc.ca>
2023-09-27 09:36:50 -0400

    Ajout des Nugets. Ajout des propriétés pour les 3 premieres entités. Fichier de constantes.

--- WizardRecords.Core/Domain/Entities/Album.cs
+++ WizardRecords.Core/Domain/Entities/Album.cs
@@ -4,10 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using static WizardRecords.Core.Data.Constants;
+
 namespace WizardRecords.Core.Domain.Entities {
     public class Album {
         public int AlbumId { get; set; }
-        public string? Title { get; set; }
         public int ArtistId { get; set; }
+        public int LabelId { get; set; }
+        public string? Title { get; set; } // TODO: Remove nullability
+        public string? CatalogNumber { get; set; } // TODO: Remove nullability
+        public int StockQuantity { get; set; }
+        public float Price { get; set; }
+        public Category Category { get; set; }
+        public MediaType Media { get; set; }
+        public FormatType Format { get; set; }
+        public AlbumGenre Genre { get; set; }
+        public Grade? MediaGrade { get; set; } // Category == USED only!
+        public Grade? SleeveGrade { get; set; } // Category == USED only!
+        public string? Comments { get; set; }
+
     }
 }
--- WizardRecords.Core/Domain/Entities/Artist.cs
+++ WizardRecords.Core/Domain/Entities/Artist.cs
@@ -3,8 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static WizardRecords.Core.Data.Constants;
 
 namespace WizardRecords.Core.Domain.Entities {
     public class Artist {
+        public int ArtistId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string BandName { get; set; }
+        public ArtistType Type { get; set; }
+        public ArtistGenre Section { get; set; }
+        public string ArtistName {
+            get {
+                if (Type == ArtistType.SOLO) {
+                    return $"{LastName}, {FirstName}";
+                }
+                else if (Type == ArtistType.BAND) {
+                    return $"{BandName}";
+                }
+                else {
+                    throw new Exception("Invalid artist type");
+                }
+            }
+        }
+
     }
 }
