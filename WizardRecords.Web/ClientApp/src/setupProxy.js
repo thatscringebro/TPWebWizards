9eec91dd45f7764ba777f5d13e2f62d198c12087
mduval76 <9343789@etu.cegepsth.qc.ca>
2023-10-12 20:41:47 -0400

    Arrangé les ports. Initial load à environ 5s. No EFcore

--- WizardRecords.Web/ClientApp/src/setupProxy.js
+++ WizardRecords.Web/ClientApp/src/setupProxy.js
@@ -2,10 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:43893';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:44415';
 
 const context = [
-  "/weatherforecast",
+  "/album",
 ];
 
 const onError = (err, req, resp, target) => {
