2e55c89ab897822e06879603091ecaa58ba1de30
mduval76 <9343789@etu.cegepsth.qc.ca>
2023-10-17 12:34:39 -0400

    push champs pour add product

--- WizardRecords.Web/ClientApp/src/components/AddProductForm.js
+++ WizardRecords.Web/ClientApp/src/components/AddProductForm.js
@@ -4,12 +4,20 @@ import '../styles/Home.css';
 
 function AddProductForm() {
     const [product, setProduct] = useState({
-        name: '',
+        artistName: '',
+        albumTItle: '',
+        labelName: '',
         category: '',
-        description: '',
+        media: '',
         price: '',
         image: '',
-        quantity: ''
+        mediaGrade: '',
+        sleeveGrade: '',
+        catalogNumber: '',
+        matrixNumber: '',
+        quantity: '',
+        genre: '',
+        comments: ''
     });
 
     const handleSubmit = async (e) => {
@@ -40,36 +48,58 @@ function AddProductForm() {
             <h2>Add new product</h2>
             <Form onSubmit={handleSubmit}>
                 <FormGroup>
-                    <Label for="name">Album</Label>
+                    <Label for="name">Artist name</Label>
                     <Input
                         type="text"
                         name="name"
                         id="name"
-                        placeholder="Nom du produit"
-                        value={product.name}
-                        onChange={e => setProduct({ ...product, name: e.target.value })}
+                        placeholder="Artist name"
+                        value={product.artistName}
+                        onChange={e => setProduct({ ...product, artistName: e.target.value })}
                     />
                 </FormGroup>
                 <FormGroup>
-                    <Label for="category">Catégorie</Label>
+                    <Label for="name">Album title</Label>
+                    <Input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Album title"
+                        value={product.albumTitle}
+                        onChange={e => setProduct({ ...product, albumTItle: e.target.value })}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="name">Label name</Label>
+                    <Input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Label name"
+                        value={product.labelName}
+                        onChange={e => setProduct({ ...product, labelName: e.target.value })}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="category">Category</Label>
                     <Input
                         type="text"
                         name="category"
                         id="category"
-                        placeholder="Catégorie"
+                        placeholder="Used or New"
                         value={product.category}
                         onChange={e => setProduct({ ...product, category: e.target.value })}
                     />
                 </FormGroup>
                 <FormGroup>
-                    <Label for="description">Description</Label>
+                    <Label for="category">Media</Label>
                     <Input
-                        type="textarea"
-                        name="description"
-                        id="description"
-                        placeholder="Description"
-                        value={product.description}
-                        onChange={e => setProduct({ ...product, description: e.target.value })}
+                        type="text"
+                        name="category"
+                        id="category"
+                        placeholder="Vinyl, CD, Cassette, etc"
+                        value={product.media}
+                        onChange={e => setProduct({ ...product, media: e.target.value })}
                     />
                 </FormGroup>
                 <FormGroup>
@@ -78,20 +108,9 @@ function AddProductForm() {
                         type="number"
                         name="price"
                         id="price"
-                        placeholder="Prix"
+                        placeholder="Price"
                         value={product.price}
                         onChange={e => setProduct({ ...product, price: e.target.value })}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="image">Image file name</Label>
-                    <Input
-                        type="text"
-                        name="image"
-                        id="image"
-                        placeholder="URL de l'image"
-                        value={product.image}
-                        onChange={e => setProduct({ ...product, image: e.target.value })}
                     />
                 </FormGroup>
                 <FormGroup>
@@ -100,9 +119,75 @@ function AddProductForm() {
                         type="number"
                         name="quantity"
                         id="quantity"
-                        placeholder="Quantité"
+                        placeholder="Quantity"
                         value={product.quantity}
                         onChange={e => setProduct({ ...product, quantity: e.target.value })}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="image">Image file name</Label>
+                    <Input
+                        type="text"
+                        name="image"
+                        id="image"
+                        placeholder="File name only"
+                        value={product.image}
+                        onChange={e => setProduct({ ...product, image: e.target.value })}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="mediaGRade">Media grade</Label>
+                    <Input
+                        type="text"
+                        name="mediaGrade"
+                        id="mediaGrade"
+                        placeholder="For used products ONLY!"
+                        value={product.mediaGrade}
+                        onChange={e => setProduct({ ...product, mediaGrade: e.target.value })}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="sleeveGrade">Sleeve grade</Label>
+                    <Input
+                        type="text"
+                        name="sleeveGrade"
+                        id="sleeveGrade"
+                        placeholder="For used products ONLY!"
+                        value={product.sleeveGrade}
+                        onChange={e => setProduct({ ...product, sleeveGrade: e.target.value })}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="catalogNumber">Catalog number</Label>
+                    <Input
+                        type="text"
+                        name="catalogNumber"
+                        id="catalogNumber"
+                        placeholder="For used products ONLY!"
+                        value={product.catalogNumber}
+                        onChange={e => setProduct({ ...product, catalogNumber: e.target.value })}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="matrixNumber">Matrix number</Label>
+                    <Input
+                        type="text"
+                        name="matrixNumber"
+                        id="matrixNumber"
+                        placeholder="For used products ONLY!"
+                        value={product.matrixNumber}
+                        onChange={e => setProduct({ ...product, matrixNumber: e.target.value })}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="description">Comments</Label>
+                    <Input
+                        type="textarea"
+                        name="description"
+                        id="description"
+                        placeholder="Comments"
+                        value={product.comments}
+                        onChange={e => setProduct({ ...product, comments: e.target.value })}
                     />
                 </FormGroup>
                 <Button type="submit">Add product</Button>
