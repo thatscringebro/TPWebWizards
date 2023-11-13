5fd640c7ba6f72f8dd6b727e316bc1d3bead172e
Rynort <hahasnake11@hotmail.com>
2023-11-12 22:34:16 -0500

    Validation des champs de commande

--- wizard_records.react/src/components/OrderPage.jsx
+++ wizard_records.react/src/components/OrderPage.jsx
@@ -11,12 +11,51 @@ const OrderPage = () => {
     country: '',
     // Ajoutez d'autres champs si nécessaire
   });
+  const [errors, setErrors] = useState({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     // Chargez les données nécessaires pour la page de commande, si nécessaire
   }, []);
+
+  const countries = [
+    "France", "Canada", "United State", "Germany", "United Kingdom",
+  ];
+
+  const validate = (data) => {
+    let errors = {};
+  
+    // Vérification du nom complet
+    if (!data.fullName.trim()) errors.fullName = "The full name is required";
+  
+    // Vérification de l'email
+    if (!data.email.trim()) {
+      errors.email = "Email required";
+    } else if (!data.email.includes('@')) {
+      errors.email = "invalid Email";
+    }
+  
+    // Vérification de l'adresse
+    if (!data.address.trim()) errors.address = "adress required";
+  
+    // Vérification de la ville
+    if (!data.city.trim()) errors.city = "town required";
+  
+    // Vérification du code postal
+    const postalCodeRegex = /^[A-Za-z]\d[A-Za-z]\d[A-Za-z]\d$/;
+    if (!data.zipCode.trim()) {
+      errors.zipCode = "Postal Code required";
+    } else if (!postalCodeRegex.test(data.zipCode)) {
+      errors.zipCode = "Postal code invalid";
+    }
+  
+    // Vérification du pays
+    if (!data.country.trim()) errors.country = "Country required";
+  
+    return errors;
+  };
+  
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -25,7 +64,9 @@ const OrderPage = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    setLoading(true);
+    const formErrors = validate(orderData);
+    if (Object.keys(formErrors).length === 0) {
+      setLoading(true);
     try {
       const response = await fetch('/api/order', {
         method: 'POST',
@@ -44,6 +85,10 @@ const OrderPage = () => {
       setError(e);
       setLoading(false);
     }
+    }
+    else {
+        setErrors(formErrors);
+      }
   };
 
   if (loading) return <div>Chargement...</div>;
@@ -103,16 +148,28 @@ const OrderPage = () => {
       </div>
       <div>
         <label htmlFor="country">Country:</label>
-        <input
-          type="text"
+        <select
           id="country"
           name="country"
           value={orderData.country}
           onChange={handleInputChange}
-        />
+        >
+          <option value="">Select a country</option>
+          {countries.map((country, index) => (
+            <option key={index} value={country}>{country}</option>
+          ))}
+        </select>
       </div>
+      {errors.fullName && <div className="error">{errors.fullName}</div>}
+      {errors.email && <div className="error">{errors.email}</div>}
+      {errors.address && <div className="error">{errors.address}</div>}
+      {errors.city && <div className="error">{errors.city}</div>}
+      {errors.zipCode && <div className="error">{errors.zipCode}</div>}
+      {errors.country && <div className="error">{errors.country}</div>}
+
+
       {/* Ajoutez d'autres champs de formulaire si nécessaire */}
-      <button type="submit">Envoyer la commande</button>
+      <button type="submit">Send</button>
     </form>
   );
 };
